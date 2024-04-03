4e1f0018420683be320c2c65a7f827fe95165c42
Nathan Fernandez <fernandeznathan891@gmail.com>
2024-04-03 19:15:32 -0400

    Added Login and Sign Up buttons to (landing)/page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,7 +1,24 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 const LandingPage = () => {
     return (
         <div>
             Landing Page (Unprotected)
+            <div>
+                <Link href="/sign-in">
+                    <Button>
+                        Login
+                    </Button>
+                </Link>
+            </div>
+            <div>
+                <Link href="/sign-up">
+                    <Button>
+                        Sign Up!
+                    </Button>
+                </Link>
+            </div>
         </div>
     );
 }
