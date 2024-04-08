618af539c0bf26309f43bd9218b171ed6f36af81
Nathan Fernandez <fernandeznathan891@gmail.com>
2024-04-08 13:48:55 -0400

    Minor readbility change

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -6,7 +6,6 @@ const DashboardPage = () => {
     return (
         <div>
             <p>Dashboard Page (Protected)</p>
-            {/* ADDING USERBUTTON AND SETTING ROUTE TO BE DEFAULT PAGE AFTER SIGNING OUT */}
                
         </div>
         
