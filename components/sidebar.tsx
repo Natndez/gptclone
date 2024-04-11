e584c681cfeff44afd02fc7eedf42872a306d80a
Nathan Fernandez <fernandeznathan891@gmail.com>
2024-04-11 13:10:32 -0400

    Updated logo color in route r: @/components/sidebar.tsx

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -18,7 +18,7 @@ const routes = [
         label: "Dashboard",
         icon: LayoutDashboard,
         href: "/dashboard",
-        color: "text-sky-500",
+        color: "text-pink-500",
     },
 ];
 
