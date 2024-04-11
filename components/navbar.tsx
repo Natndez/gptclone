b86535b06b671f40cbe2e21b7349a2c7e0b83e22
Nathan Fernandez <fernandeznathan891@gmail.com>
2024-04-10 20:29:59 -0400

    minor update

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,6 +4,8 @@ import { Button } from "@/components/ui/button"
 import { UserButton } from "@clerk/nextjs";
 import { Menu } from "lucide-react"
 
+// Figure out some aesthetic details
+
 const Navbar = () => {
     return (
         <div className="flex items-center p-4">
