297cef74b9a511cd41f977f5aad4274417a408ef
Nathan Fernandez <fernandeznathan891@gmail.com>
2024-04-11 12:59:22 -0400

    Created initial routes array and displaying to sidebar with a map

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -7,9 +7,20 @@ import { Montserrat } from "next/font/google";
 
 // Personal imports
 import { cn } from "@/lib/utils";
+import { LayoutDashboard } from "lucide-react";
 
 // Monterrat font
 const montserrat = Montserrat({ weight: "600", subsets: ["latin"]});
+
+// Setting up array of objects for our routes
+const routes = [
+    {
+        label: "Dashboard",
+        icon: LayoutDashboard,
+        href: "/dashboard",
+        color: "text-sky-500",
+    },
+];
 
 const Sidebar = () => {
     return (
@@ -25,11 +36,32 @@ const Sidebar = () => {
                         />
                     </div>
                     {/* Using 'cn' to use our montserrat font... use the following convention to do so while still using shadcn classes */}
+                    {/* cn allows for dynamic class name's for tailwind elements */}
                     <h1 className={cn("text-2xl font-bold",
                         montserrat.className)}>
                         Genie
                     </h1>
                 </Link>
+                <div className="space-y-1">
+                    {/* Setting up routes map */}
+                    {routes.map((route) => (
+                        <Link
+                            href={route.href}
+                            key={route.href}
+                            className="text-sm group flex p-3 w-full
+                            justify-start font-medium cursor-pointer
+                            hover:text-white hover:bg-white/10 rounder-lg
+                            transition"
+                        >
+                            {/* Setting up logo and label for each route */}
+                            <div className="flex items-center flex-1">
+                                <route.icon className={cn("h-5 w-5 mr-3", 
+                                route.color)} />
+                                {route.label}
+                            </div>
+                        </Link>
+                    ))}
+                </div>
             </div>
         </div>
     );
