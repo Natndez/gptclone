1ff522441f3452da34ce84ee1a17974ebbd7f6e3
Nathan Fernandez <fernandeznathan891@gmail.com>
2024-04-19 13:51:11 -0400

    Added foundation for chat page

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -91,7 +91,7 @@ const Sidebar = () => {
                             key={route.href}
                             // Using cn to dynamically assign classes (highlights current tab)
                             className={cn("text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition",
-                            pathname === route.href ? "text-white bg-white/10" : "text-zinc-400" // To highlight current route    
+                            pathname === route.href ? "text-white bg-white/10" : "text-zinc-300" // To highlight current route    
                             )}
                         >
                             {/* Setting up logo and label for each route */}
