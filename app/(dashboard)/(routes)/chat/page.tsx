7b1238f1e87552ace5e143d58524c6d1b17d9514
Nathan Fernandez <fernandeznathan891@gmail.com>
2024-04-22 16:34:16 -0400

    Set up Heading component for /chat page

--- app/(dashboard)/(routes)/chat/page.tsx
+++ app/(dashboard)/(routes)/chat/page.tsx
@@ -1,9 +1,17 @@
 import { Heading } from "@/components/heading"
+import { MessageCircle } from "lucide-react";
 
 const ChatPage = () => {
     return (
         <div>
-            <Heading />
+            {/* Assigning Props to conform to our HeadingProps interface */}
+            <Heading
+                title="Chat"
+                description="Ask Genie anything"
+                icon={MessageCircle}
+                iconColor="text-violet-500"
+                bgColor="bg-violet-500/10" 
+            />
         </div>
     );
 }
--- components/heading.tsx
+++ components/heading.tsx
@@ -1,7 +1,39 @@
-export const Heading = () => {
+import { cn } from "@/lib/utils";
+import { LucideIcon } from "lucide-react";
+
+// Setting up interface for Heading Properties
+interface HeadingProps {
+    title: string;
+    description: string;
+    icon: LucideIcon;
+    iconColor?: string; // iconColor is an optional property
+    bgColor?: string;   // bgColor as well
+}
+
+export const Heading = ({
+    title,
+    description,
+    icon: Icon, // Remapping to captial I
+    iconColor,
+    bgColor
+}: HeadingProps) => {
+    
     return (
-        <div>
-            Heading Component
-        </div>
+        // Wrapping in fragment
+        <>
+            <div className="px-4 lg:px-8 flex items-center gap-x-3 mb-8">
+                <div className={cn("p-2 w-fit rounded-md", bgColor)}>
+                    <Icon className={cn("w-10 h-10", iconColor)} />
+                </div>
+                <div>
+                    <h2 className="text-3xl font-bold">
+                        {title}
+                    </h2>
+                    <p className="text-sm text-muted-foreground">
+                        {description}
+                    </p>
+                </div>
+            </div>    
+        </>
     )
 }
