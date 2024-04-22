12b54cfb95d9ae149b7c6ef4d1e17a08cfa672e3
Nathan Fernandez <fernandeznathan891@gmail.com>
2024-04-22 16:13:22 -0400

    Set up heading component

--- app/(dashboard)/(routes)/chat/page.tsx
+++ app/(dashboard)/(routes)/chat/page.tsx
@@ -1,7 +1,9 @@
+import { Heading } from "@/components/heading"
+
 const ChatPage = () => {
     return (
         <div>
-            <h1>Chat page!</h1>
+            <Heading />
         </div>
     );
 }
