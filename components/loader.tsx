915ca15418b3d7e6436b13054320d693b8207cc1
Nathan Fernandez <fernandeznathan891@gmail.com>
2024-04-24 13:58:08 -0400

    Updated loader image

--- components/loader.tsx
+++ components/loader.tsx
@@ -7,7 +7,7 @@ export const Loader = () => {
                 <Image
                     alt="logo"
                     fill
-                    src="/logo.png"
+                    src="/loader.png"
                 />
             </div>
             <p className="text-sm text-muted-foreground">
