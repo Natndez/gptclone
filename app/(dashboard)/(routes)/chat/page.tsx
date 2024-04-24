b97e7cfe93b01df3eb3106fc576c2fa8349f86fa
Nathan Fernandez <fernandeznathan891@gmail.com>
2024-04-24 18:21:13 -0400

    Styling messages based on role (chat page)

--- app/(dashboard)/(routes)/chat/page.tsx
+++ app/(dashboard)/(routes)/chat/page.tsx
@@ -17,6 +17,7 @@ import { Empty } from "@/components/empty";
 import { Loader } from "@/components/loader";
 
 import { formSchema } from "./constants";
+import { cn } from "@/lib/utils";
 
 
 
@@ -138,8 +139,7 @@ const ChatPage = () => {
                 </div>
                 <div className="space-y-4 mt-4">
                     {/* Setting up conditional for what renders */}
-                    {/* Use true instead of isLoading to verify functionality */}
-                    {isLoading && (
+                    {isLoading && ( // Use true instead of isLoading to verify functionality 
                         <div className="p-8 rounded-lg w-full lfex items-center justify-center bg-muted">
                             <Loader />
                         </div>
@@ -147,8 +147,13 @@ const ChatPage = () => {
                     {messages.length === 0 && !isLoading && ( <Empty label="Make your first wish - Unleash the Genie" /> )}
                     <div className="flex flex-col-reverse gap-y-4">
                         {messages.map((message) => (
-                            <div key={message.content}>
-                                {message.content}
+                            <div 
+                                key={message.content}
+                                className={cn("p-8 w-full flex items-start gap-x-8 rounded-lg",
+                                    message.role === "user" ? "bg-white border border-black/10" : "bg-muted"
+                                )}
+                            >
+                                    {message.content}
                             </div>
                         ))}
                     </div>
