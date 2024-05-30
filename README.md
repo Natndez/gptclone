38f02de39b7bbbf9efd5794bc29944ea3a2cc2c4
Nathan Fernandez <fernandeznathan891@gmail.com>
2024-05-30 14:03:16 -0400

    Connection to Supabase now working

--- README.md
+++ README.md
@@ -18,6 +18,7 @@ This is an application intended to be a clone of ChatGPT with an interface I pre
 ### Database Information
 - Currently using Supabase (Linked with GitHub Account)
     - https://supabase.com/ 
+    - use `npx prisma db push` to update db
 - Could use PlanetScale  (Simplest solution but expensive, Antonio uses this, better to find my own option)
     - https://planetscale.com/ 
 - Research more options if these two aren't realistic
--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -4,9 +4,10 @@ declare global {
     var prisma: PrismaClient | undefined;
 };
 
+// There is a chance lots of prisma clients are initialized everytime we hot reload with next. Thus doing this so hot reloading so we can have multiple prisma contexts active.
 const prismadb = globalThis.prisma || new PrismaClient();
 
-// Only do for development env
+// Only do in development env
 if (process.env.NODE_ENV !== "production") {
     globalThis.prisma = prismadb;
 }
