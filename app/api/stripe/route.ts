ebe29bcdf9f96c966fffebc02682049f720b8dfa
Nathan Fernandez <fernandeznathan891@gmail.com>
2024-07-05 14:12:07 -0400

    Finished stripe checkout page settings

--- app/api/stripe/route.ts
+++ app/api/stripe/route.ts
@@ -37,7 +37,36 @@ export async function GET(){
             return new NextResponse(JSON.stringify({ url: stripeSession.url }));
         }
 
-        // TODO: Set up checkout page next 
+        const stripeSession = await stripe.checkout.sessions.create({
+            success_url: settingsUrl,
+            cancel_url: settingsUrl,
+            payment_method_types: ["card"], // Settings for allowed payment types 
+            mode: "subscription", // Payment mode
+            billing_address_collection: "auto",
+            customer_email: user.emailAddresses[0].emailAddress, // User comes from clerk
+            line_items: [
+                {
+                    price_data: {
+                        currency: "USD",
+                        product_data: {
+                            name: "Genie Pro",
+                            description: "Unlimited AI Generations"
+                        },
+                        unit_amount: 2000,  // 2000 is equivalent to 20 dollars
+                        recurring: {
+                            interval: "month", // Payment happens monthly
+                        }, 
+                    },
+                    quantity: 1,
+                }
+            ],
+            metadata: {
+                userId, // passing in userId as metadata ensures the system knows which user is getting the premium subscription
+            },
+        });
+
+        // Return this stripeSession URL
+        return new NextResponse(JSON.stringify({ url: stripeSession.url }));
 
     } catch (error) {
         console.log("[STRIPE_ERROR] ", error);
