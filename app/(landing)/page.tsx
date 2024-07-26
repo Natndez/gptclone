6de76bfaf0064d6f06ba4d6fb1109395bdabe9b9
Nathan Fernandez <fernandeznathan891@gmail.com>
2024-07-26 15:04:05 -0500

    Set up file structure for landing page navbar

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,23 +1,12 @@
+import { LandingNavbar } from "@/components/landing-navbar";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 // The Login/Sign-Up page for Genie
 const LandingPage = () => {
     return (
-        <div>
-            Landing Page (Open)
-            <div>
-                <Link href="/sign-in">
-                    <Button>
-                        Login
-                    </Button>
-                </Link>
-                <Link href="/sign-up">
-                    <Button>
-                        Register
-                    </Button>
-                </Link>
-            </div>
+        <div className="h-full">
+            <LandingNavbar />
         </div>
     );
 }
---
+++ components/landing-navbar.tsx
@@ -0,0 +1,7 @@
+export const LandingNavbar = () => {
+    return (
+        <div>
+            This is the navbar
+        </div>
+    );
+}
