c44ec032c1fe621e47137b20a0979a899a000728
Nathan Fernandez <fernandeznathan891@gmail.com>
2024-07-31 11:26:11 -0500

    Finished landing-hero

--- components/landing-hero.tsx
+++ components/landing-hero.tsx
@@ -1,6 +1,10 @@
 "use client";
 
 import { useAuth } from "@clerk/nextjs";
+import Link from "next/link";
+
+import TypewriterComponent from "typewriter-effect";
+import { Button } from "@/components/ui/button";
 
 export const LandingHero = () => {
     // True if user is signed in
@@ -8,7 +12,39 @@ export const LandingHero = () => {
 
     return (
         <div className="text-white font-bold py-36 text-center space-y-5">
-            This is the hero for now
+            <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl space-y-5 font-extrabold">
+                <h1>
+                    The Magic To Create
+                </h1>
+                <div className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+                    <TypewriterComponent 
+                        options={{
+                            strings: [
+                                "Conversation.",
+                                "Photos.",
+                                "Videos.",
+                                "Music.",
+                                "Code.",
+                            ],
+                            autoStart: true,
+                            loop: true,
+                        }}
+                    />
+                </div>
+            </div>
+            <div className="text-sm md:text-xl font-light text-zinc-400">
+                    Create content using the magic of AI.
+            </div>
+            <div>
+                <Link href={isSignedIn ? "/dashboard" : "/sign-up"}>
+                    <Button variant="premium" className="md:text-lg p-4 md:p-6 rounded-full font-semibold">
+                        Start Generating For Free
+                    </Button>
+                </Link>
+            </div>
+            <div className="text-zinc-400 text-xs font-normal">
+                Credit card not required.
+            </div>
         </div>
     );
 }
