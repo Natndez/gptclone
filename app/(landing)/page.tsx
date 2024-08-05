2b77d5ae7fe0b9ec35440d55b42646b7b12991df
Nathan Fernandez <fernandeznathan891@gmail.com>
2024-08-05 16:47:14 -0500

    Began LandingContent.tsx

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,3 +1,4 @@
+import { LandingContent } from "@/components/landing-content";
 import { LandingHero } from "@/components/landing-hero";
 import { LandingNavbar } from "@/components/landing-navbar";
 
@@ -11,6 +12,7 @@ const LandingPage = () => {
         <div className="h-full">
             <LandingNavbar />
             <LandingHero />
+            <LandingContent />
         </div>
     );
 }
