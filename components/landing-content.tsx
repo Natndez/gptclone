3749cdafeeb3cf230739d0f2d601f9da670826a4
Nathan Fernandez <fernandeznathan891@gmail.com>
2024-08-07 14:25:45 -0400

    Finished testimonials section

--- components/landing-content.tsx
+++ components/landing-content.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
 // Array of objects containing testimonials
 const testimonials = [
     {
@@ -47,7 +49,21 @@ export const LandingContent = () => {
                 Testimonials
             </h2>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-
+                {testimonials.map((item) => (
+                    <Card key={item.description} className="bg-[#192339] border-none text-white">
+                        <CardHeader>
+                            <CardTitle className="flex items-center gap-x-2">
+                                <div>
+                                    <p className="text-lg">{item.name}</p>
+                                    <p className="text-zinc-400 text-sm">{item.title}</p>
+                                </div>
+                            </CardTitle>
+                            <CardContent className="pt-4 px-0">
+                                {item.description}
+                            </CardContent>
+                        </CardHeader>
+                    </Card>
+                ))}
             </div>
         </div>
     )
